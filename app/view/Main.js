f1107fc696c402e46ded3c27bb7f190f9214787e
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-04-22 18:10:53 +0200

    added contact and contactlist views

--- app/view/Main.js
+++ app/view/Main.js
@@ -3,48 +3,46 @@ Ext.define('Agenda.view.Main', {
     xtype: 'main',
     requires: [
         'Ext.TitleBar',
-        'Ext.Video'
+        'Agenda.view.ContactList',
+        'Agenda.view.Contact'
     ],
+
     config: {
         tabBarPosition: 'bottom',
 
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
+        items: [{
+            title: 'Agenda',
+            iconCls: 'home',
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
             },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
 
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
-            }
-        ]
+            items: [{
+                xtype: 'titlebar',
+                docked: 'top',
+                title: 'Contact List'
+            } , {
+                xtype: 'contactList'
+            }]
+        } , {
+            title: 'Add Contact',
+            iconCls: 'action',
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+
+            items: [{
+                docked: 'top',
+                xtype: 'titlebar',
+                title: 'Contact'
+            } , {
+                xtype: 'contact'
+            } , {
+                xtype: 'button',
+                text: 'Add Contact'
+            }]
+        }]
     }
 });
