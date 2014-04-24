4de7c7d27e8cdb5121333dd631da8eff9182c325
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-04-24 18:17:59 +0200

    added properties name

--- app/view/Contact.js
+++ app/view/Contact.js
@@ -1,10 +1,11 @@
 Ext.define('Agenda.view.Contact', {
     extend: 'Ext.form.Panel',
-    xtype: 'contact',
+    xtype: 'contactform',
     requires: ['Ext.field.Email'],
 
     config: {
         flex: 1,
+        record: 'Agenda.model.Contact',
         items: [{
             xtype: 'textfield',
             name: 'name',
@@ -25,6 +26,37 @@ Ext.define('Agenda.view.Contact', {
             xtype: 'textfield',
             name: 'website',
             label: 'WebSite'
+        } , {
+            xtype: 'button',
+            name: 'insertContact',
+            flex: 1,
+            text: 'Insert',
+            ui: 'confirm-round',
+            margin: '10 10 10 10'
+        } , {
+            xtype: 'container',
+            name: 'buttonContainer',
+            padding: '10 0 0 0',
+            hidden: true,
+            layout: {
+                type: 'hbox',
+                align: 'stretch'
+            },
+            items: [{
+                xtype: 'button',
+                name: 'deleteContact',
+                margin: '0 10 0 10',
+                flex: .5,
+                ui: 'decline-round',
+                text: 'Delete'
+            } , {
+                xtype: 'button',
+                name: 'updateContact',
+                margin: '0 10 0 0',
+                ui: 'action-round',
+                flex: .5,
+                text: 'Update'
+            }]
         }]
     }
 });
