847931ee6b8240637093d43d66d28d527f9dabd4
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-04-24 18:18:11 +0200

    added name as property

--- app/view/ContactList.js
+++ app/view/ContactList.js
@@ -3,15 +3,16 @@ Ext.define('Agenda.view.ContactList', {
     xtype: 'contactList',
     config: {
         flex: 1,
-        data: [{name: 'Asder', surname: 'Loller', address: 'Via tal dei tali', email: 'asd@lol.com', website: 'www.loller.com'}]
-    },
-
-    itemTpl: [
-        '<div>',
-            '<p>{name} {surname}</p>',
-            '<p>Address: {address}</p>',
-            '<p>Email: {email}</p>',
-            '<p>Website: {website}</p>',
-        '</div>'
-    ].join('')
+        emptyText: 'No contacts available',
+        deferEmptyText: false,
+        store: 'contactList',
+        itemTpl: [
+            '<div>',
+                '<p>{name} {surname}</p>',
+                '<p>Address: {address}</p>',
+                '<p>Email: {email}</p>',
+                '<p>Website: {website}</p>',
+            '</div>'
+        ].join('')
+    }
 });
