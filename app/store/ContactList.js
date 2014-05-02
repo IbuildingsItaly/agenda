f5819c6738c2b3534a0cfd9009bd11043bb968c3
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-05-02 23:40:22 +0200

    added localstorage proxy as a dependency

--- app/store/ContactList.js
+++ app/store/ContactList.js
@@ -1,9 +1,14 @@
 Ext.define('Agenda.store.ContactList', {
     extend: 'Ext.data.Store',
+    requires: ['Ext.data.proxy.LocalStorage'],
     config: {
         storeId: 'contactList',
         model: 'Agenda.model.Contact',
         autoLoad: true,
-        autoSync: true
+        autoSync: true,
+        proxy: {
+            type: 'localstorage',
+            id: 'agenda'
+        }
     }
 });
