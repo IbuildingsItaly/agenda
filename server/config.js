b3956a31b9a8170b9fe3ecf6951bf882c846ea51
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-05-16 18:08:35 +0200

    untracked server configs

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    apiKey: '<GCM API KEY>',
-    deviceKey: '<DEVICE ID>'
+    apiKey: 'AIzaSyBWGR9NXeabQIYgwazyEt7evhMhp44Aims',
+    deviceKey: 'APA91bF7JIDLSBothBK38rBMP87rWsXH8T0T93cVthkfnV5UaSswVYHHUmouhqkcpAm2ofiksvtPKjnv1YayCnNQEOCHO1M20Ectsp1OtRB6828TN7EwRgE5um7Q99c3kV3QX4VT42zma1fbYrtscUyyX6GOoU659Q'
 };
