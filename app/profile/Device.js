4cbab5e02afaeae48f56905956d10f4d845ee7c8
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-05-19 10:37:30 +0200

    added stable draft for profile Device

--- app/profile/Device.js
+++ app/profile/Device.js
@@ -1,3 +1,48 @@
-/**
- * Created by wilk on 16/05/14.
- */
+Ext.define('Agenda.profile.Device', {
+    extend: 'Ext.app.Profile',
+
+    DEVICE_SENDER_ID: 'GCM_PROJECT_NUMBER',
+
+    isActive: function () {
+        return Ext.os.is.Tablet || Ext.os.is.Phone;
+    },
+
+    onRegisterSuccess: function (token) {
+        Ext.Msg.alert('TOKEN', token);
+    },
+    onRegisterFailure: function (error) {
+        Ext.Msg.alert('ERROR', error);
+    },
+    onNotificationsReceived: function (notifications) {
+        switch (notifications.event) {
+            case 'registered':
+                if(notifications.regid.length > 0) {
+                    //Ext.Msg.alert('NOTIFICATION :: REGISTERED', notifications.regid);
+                }
+                break;
+            case 'message':
+                var store = Ext.getStore('contactList');
+
+                store.add(notifications.payload.user);
+                break;
+            case 'error':
+                Ext.Msg.alert('NOTIFICATION :: ERROR', notifications.msg);
+                break;
+            default:
+                Ext.Msg.alert('NOTIFICATION', 'Undefined notification');
+                break;
+        }
+    },
+
+    launch: function () {
+        var me = this;
+
+        Ext.device.Push.register({
+            type: Ext.device.Push.ALERT | Ext.device.Push.BADGE | Ext.device.Push.SOUND,
+            senderID: me.DEVICE_SENDER_ID,
+            success: me.onRegisterSuccess,
+            failure: me.onRegisterFailure,
+            received: me.onNotificationsReceived
+        });
+    }
+});
